87316a09709a664cb159efb4597506360164057a
aditya142003 <adityabhatnagar142003@gmail.com>
2023-04-03 15:21:40 +0530

    minor change

--- src/Components/ImageArea.js
+++ src/Components/ImageArea.js
@@ -10,6 +10,8 @@ import CIcon from "./Images/Icons/C++Icon.png";
 
 function ImageArea() {
   const [mousePos, setMousePos] = useState({});
+  const [opacityLeft, setopacityLeft] = useState(70);
+  const [opacityRight, setopacityRight] = useState(70);
   let windowHeight = window.innerHeight;
   let windowWidth = window.innerWidth;
 
@@ -25,33 +27,49 @@ function ImageArea() {
     ) {
       original.style.setProperty("--mask-grad", "50%");
       original.style.setProperty("--mask-size", "160%");
+      setopacityLeft(100);
+      setopacityRight(70);
     } else if (
       mousePos?.x > windowWidth / 2 - (windowWidth * 30) / 100 &&
-      mousePos?.x < windowWidth / 2 + (windowWidth * 15) / 100 &&
+      mousePos?.x < windowWidth / 2 + (windowWidth * 12) / 100 &&
       mousePos?.y > (windowHeight * 40) / 100 &&
       mousePos?.y < (windowHeight * 90) / 100
     ) {
       original.style.setProperty("--mask-grad", "50%");
       original.style.setProperty("--mask-size", "103%");
+      setopacityLeft(70);
+      setopacityRight(70);
     } else if (
-      mousePos?.x > windowWidth / 2 + (windowWidth * 15) / 100 &&
+      mousePos?.x > windowWidth / 2 + (windowWidth * 12) / 100 &&
       mousePos?.y > (windowHeight * 40) / 100 &&
       mousePos?.y < (windowHeight * 90) / 100
     ) {
       original.style.setProperty("--mask-grad", "50%");
       original.style.setProperty("--mask-size", "70%");
+      setopacityLeft(70);
+      setopacityRight(100);
     } else {
       original.style.setProperty("--mask-grad", "50%");
       original.style.setProperty("--mask-size", "103%");
+      setopacityLeft(70);
+      setopacityRight(70);
     }
     window.addEventListener("mousemove", handleMouseMove);
   });
+  let name = document.getElementById("name");
+  setTimeout(() => {
+    name.classList.remove("hidden");
+  }, 90);
 
   return (
-    <>
+    <div style={{width:"100vw", display:"flex", flexDirection:"row"}}>
+      <div id="name" className="name hidden">Aditya Bhatnagar</div>
       <div className="mainContainer">
-        <div className="textTag textLeft">
-          <div>FRONTEND DEVELOPER</div>
+        <div
+          className="textTag textLeft"
+          style={{ opacity: `${opacityLeft}%`, transition: "0.7s ease" }}
+        >
+          <div>frontend developer</div>
           <div>
             <img src={HtmlIcon} className="icons"></img>
             <img src={CssIcon} className="icons"></img>
@@ -63,14 +81,17 @@ function ImageArea() {
           <img src={Painted} className="imageBase"></img>
           <img src={Original} className="imageBase"></img>
         </div>
-        <div className="textTag textRight">
-          <div>PROGRAMMER</div>
+        <div
+          className="textTag textRight"
+          style={{ opacity: `${opacityRight}%`, transition: "0.7s ease" }}
+        >
+          <div>programmer</div>
           <div>
             <img src={CIcon} className="icons"></img>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
