a335fcdcf02672a7d1c8d446c197b3c90c942dd0
aditya142003 <adityabhatnagar142003@gmail.com>
2023-04-05 23:00:26 +0530

    about page structure completed

---
+++ src/Components/About.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import "./Style/About.css";
+import Typed from "typed.js";
+import Udaipur from "./Images/Udaipur.jpg";
+import Jaipur from "./Images/Jaipur.jpg";
+function About() {
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "React.js ^1000",
+        "JavaScript ^1000",
+        "C++ ^1000",
+        "DSA ^1000",
+        "HTML ^1000",
+        "CSS ^1000",
+        "Git ^1000",
+      ],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+  let aboutImageU = document.getElementById("aboutImageU");
+  let aboutImageJ = document.getElementById("aboutImageJ");
+  let techStack = document.getElementById("techStack");
+  function handleUdaipur() {
+    aboutImageU.style.setProperty("left", "0");
+    techStack.style.setProperty("left", "320px");
+  }
+  function handleLeaveU() {
+    aboutImageU.style.setProperty("left", "320px");
+    techStack.style.setProperty("left", "0");
+  }
+  function handleJaipur() {
+    aboutImageJ.style.setProperty("left", "0");
+    techStack.style.setProperty("left", "320px");
+  }
+  function handleLeaveJ() {
+    aboutImageJ.style.setProperty("left", "320px");
+    techStack.style.setProperty("left", "0");
+  }
+  return (
+    <div>
+      <p
+        style={{
+          fontSize: "70px",
+          width: "100px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "30px",
+          marginBottom: "30px",
+        }}
+      >
+        .about
+      </p>
+      <div className="about">
+        <div className="aboutText">
+          <p className="firstLine">
+            <pre>
+              {/* prettier-ignore */}
+              <p> Hi! I'm Aditya frontend web developer </p>
+              {/* prettier-ignore */}
+              <p>born in <span onMouseEnter={handleUdaipur} onMouseLeave={handleLeaveU} className="city" ><a href="https://www.tourism.rajasthan.gov.in/udaipur.html" target="_blank">Udaipur</a></span>, based in <span onMouseEnter={handleJaipur} onMouseLeave={handleLeaveJ} className="city"><a href="https://www.tourism.rajasthan.gov.in/jaipur.html" target="_blank">Jaipur</a></span>, India </p>
+            </pre>
+          </p>
+          <pre>
+            <p className="secondLine">
+              {/* prettier-ignore */}
+              <p>  I create virtual homes for</p>
+              {/* prettier-ignore */}
+              <p> my clients on world wide web.</p>
+            </p>
+          </pre>
+
+          <p className="thirdLine">
+            <pre>
+              <p>Currently I'm exploring smart contracts</p>
+              {/* prettier-ignore */}
+              <p> and its  intergation  with  frontend.</p>
+            </pre>
+          </p>
+        </div>
+        <div
+          style={{
+            width: "20vw",
+            height: "50vh",
+            display: "grid",
+            placeContent: "center",
+            position: "relative",
+            overflow: "hidden",
+          }}
+        >
+          <div className="techStack" id="techStack">
+            <span ref={el} />
+          </div>
+          <div style={{ height: "25vh" }}>
+            <img src={Udaipur} id="aboutImageU" className="aboutImage"></img>
+            <img src={Jaipur} id="aboutImageJ" className="aboutImage"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/Components/MainArea.js
+++ src/Components/MainArea.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./Style/MainArea.css";
-import Typed from "typed.js";
+
 import Painted from "./Images/Painted.png";
 import Original from "./Images/Original.png";
-import Udaipur from "./Images/Udaipur.jpg";
-import Jaipur from "./Images/Jaipur.jpg";
 import ReactIcon from "./Images/Icons/ReactIcon.png";
 import HtmlIcon from "./Images/Icons/HtmlIcon.png";
 import CssIcon from "./Images/Icons/CssIcon.png";
 import JSIcon from "./Images/Icons/JSIcon.png";
 import CIcon from "./Images/Icons/C++Icon.png";
+import About from "./About";
 
 function ImageArea() {
   const [mousePos, setMousePos] = useState({});
@@ -66,21 +65,6 @@ function ImageArea() {
     textRight.classList.add("aniright");
   }, 700);
 
-  const el = React.useRef(null);
-  React.useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ["React.js ^1000", "JavaScript ^1000", "C++ ^1000", "DSA ^1000", "HTML ^1000", "CSS ^1000", "Git ^1000"],
-      typeSpeed: 100,
-      backSpeed: 100,
-      loop: true,
-    });
-
-    return () => {
-      typed.destroy();
-      // Destroy Typed instance during cleanup to stop animation
-    };
-  }, []);
-
   return (
     <div style={{ width: "100vw", display: "flex", flexDirection: "row" }}>
       <div className="mainContainer">
@@ -113,61 +97,7 @@ function ImageArea() {
             </div>
           </div>
         </div>
-        <p
-          style={{
-            fontSize: "65px",
-            width: "200px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: "30px",
-            marginBottom: "30px",
-          }}
-        >
-          .about
-        </p>
-        <div className="about">
-          <div className="aboutText">
-            <p className="firstLine">
-              <pre>
-                {/* prettier-ignore */}
-                <p> Hi! I'm Aditya frontend web developer </p>
-                {/* prettier-ignore */}
-                <p>born in Udaipur, based in Jaipur, India </p>
-              </pre>
-            </p>
-            <pre>
-              <p className="secondLine">
-                {/* prettier-ignore */}
-                <p>  I create virtual homes for</p>
-                {/* prettier-ignore */}
-                <p> my clients on world wide web.</p>
-              </p>
-            </pre>
-
-            <p className="thirdLine">
-              <pre>
-                <p>Currently I'm exploring smart contracts</p>
-                {/* prettier-ignore */}
-                <p> and its  intergation  with  frontend.</p>
-              </pre>
-            </p>
-          </div>
-          <div
-            style={{
-              width: "20vw",
-              height: "50vh",
-              display: "grid",
-              placeContent: "center",
-            }}
-          >
-            <span className="techStack"  ><span ref={el}/></span>
-            {/* <img src={Udaipur} className="aboutImage"></img>
-            <br />
-            <img src={Jaipur} className="aboutImage"></img>
-            <img></img>
-            <img></img> */}
-          </div>
-        </div>
+        <About />
       </div>
     </div>
   );
